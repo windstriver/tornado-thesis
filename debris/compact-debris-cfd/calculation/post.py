b2e2ca381b2e72ec970227308dbdcfcbeb603177
windstriver <yungwong.seu@gmail.com>
2015-10-29 15:17:01 +0800

    compact debris using cfd method -- completed

--- debris/compact-debris-cfd/calculation/post.py
+++ debris/compact-debris-cfd/calculation/post.py
@@ -2,6 +2,8 @@
 from case1 import *
 from case2 import *
 from case3 import *
+import case4
+import case5
 
 from matplotlib.backends.backend_pdf import FigureCanvasPdf as FigureCanvas
 from matplotlib.figure import Figure
@@ -25,3 +27,24 @@ ax2.set_ylabel(r'$u_z (\mathrm{m/s})$')
 ax2.legend(handles=[line_u_z1, line_u_z2, line_u_z3])
 
 fig.savefig('velocity_history.pdf')
+
+fig2 = Figure(tight_layout=True)
+canvas2 = FigureCanvas(fig2)
+ax1 = fig2.add_subplot(1, 2, 1)
+line_u_t3, = ax1.plot(time3, u_t3, '', label=r'Case 3')
+line_u_t4, = ax1.plot(case4.time, case4.u_t, '--', label=r'Case 4')
+line_u_t5, = ax1.plot(case5.time, case5.u_t, '-.', label=r'Case 5')
+ax1.set_xlabel(r'$t / \mathrm{s}$')
+ax1.set_ylabel(r'$u_t (\mathrm{m/s})$')
+ax1.legend(handles=[line_u_t3, line_u_t4, line_u_t5], loc='upper left')
+
+ax2 = fig2.add_subplot(1, 2, 2)
+line_u_z3, = ax2.plot(time3, u_z3, label=r'Case 3')
+line_u_z4, = ax2.plot(case4.time, case4.u_z, '--', label=r'Case 4')
+line_u_z5, = ax2.plot(case5.time, case5.u_z, '-.', label=r'Case 5')
+ax2.set_xlabel(r'$t / \mathrm{s}$')
+ax2.set_ylabel(r'$u_z (\mathrm{m/s})$')
+ax2.legend(handles=[line_u_z3, line_u_z4, line_u_z5])
+
+
+fig2.savefig('velocity_history2.pdf')
